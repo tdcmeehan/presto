f12d2d1d16dabb117875a6c4ea4fbd2c00ecb9aa
Tim Meehan <tdm@fb.com>
2020-06-11 12:58:23 -0700

    Add Presto JDBC URL flag to add session property overrides
    Session property overrides can be specified by the sessionProperty flag,
    with each session property separated by `;`, and each key value pair
    separated by `:`.
    
    For example:
    
    `presto://localhost:8080?sessionProperties=prop1:value1;prop2:value2`

--- presto-jdbc/src/test/java/com/facebook/presto/jdbc/TestPrestoDriverUri.java
+++ presto-jdbc/src/test/java/com/facebook/presto/jdbc/TestPrestoDriverUri.java
@@ -103,6 +103,9 @@ public class TestPrestoDriverUri
 
         // empty extra credentials
         assertInvalid("presto://localhost:8080?extraCredentials=", "Connection property 'extraCredentials' value is empty");
+
+        assertInvalid("presto://localhost:8080?sessionProperties=", "Connection property 'sessionProperties' value is empty");
+        assertInvalid("presto://localhost:8080?sessionProperties=sdf", "Connection property 'sessionProperties' value is invalid: sdf");
     }
 
     @Test(expectedExceptions = SQLException.class, expectedExceptionsMessageRegExp = "Connection property 'user' is required")
@@ -233,6 +236,16 @@ public class TestPrestoDriverUri
         PrestoDriverUri parameters = createDriverUri("presto://localhost:8080?extraCredentials=" + extraCredentials);
         Properties properties = parameters.getProperties();
         assertEquals(properties.getProperty(EXTRA_CREDENTIALS.getKey()), extraCredentials);
+    }
+
+    @Test
+    public void testUriWithSessionProperties()
+            throws SQLException
+    {
+        String sessionProperties = "sessionProp1:sessionValue1;sessionProp2:sessionValue2";
+        PrestoDriverUri parameters = createDriverUri("presto://localhost:8080?extraCredentials=" + sessionProperties);
+        Properties properties = parameters.getProperties();
+        assertEquals(properties.getProperty(EXTRA_CREDENTIALS.getKey()), sessionProperties);
     }
 
     private static void assertUriPortScheme(PrestoDriverUri parameters, int port, String scheme)
